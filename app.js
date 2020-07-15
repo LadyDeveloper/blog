6632a71818e88dd9083b5562ecbcd8dacc7254c3
LadyDeveloper <anhacorrea@hotmail.com>
2020-07-15 14:06:18 -0700

    Added database

--- app.js
+++ app.js
@@ -2,8 +2,11 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require('mongoose');
 const ejs = require("ejs");
-const _ = require('lodash');
+const Blog = require('./models/blog');
+
+mongoose.connect('mongodb://localhost:27017/blogDB', {useNewUrlParser: true, useUnifiedTopology: true})
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -16,10 +19,15 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-let posts = [];
 
 app.get('/', (req, res) => {
-  res.render('home', {homeContent: homeStartingContent, posts: posts});
+  Blog.find({}, (err, posts) => {
+    if(!err){
+      res.render('home', {homeContent: homeStartingContent, posts: posts});
+    } else {
+      console.log(err);
+    }
+  });
 });
 
 app.get('/about', (req, res) => {
@@ -38,30 +46,33 @@ app.post('/compose', (req, res) => {
 
   const post = {
     title: req.body.postTitle,
-    content: req.body.postBody
+    author: req.body.author,
+    body: req.body.postBody
   };
-  posts.push(post);
-  res.redirect('/');
-})
 
-
-app.get('/posts/:postName', (req, res) => {
-
-  const requestedTitle = _.lowerCase(req.params.postName);
-
-  posts.forEach( post => {
-    const storedTitle = _.lowerCase(post.title);
-
-    if(requestedTitle === storedTitle){
-      res.render('post', { title: post.title, content: post.content });
+  Blog.create(post, (err, post) => {
+    if(!err){
+      console.log('Post created successfuly!');
+      res.redirect('/');
     } else {
-      console.log('Not a match!');
+      console.log(err);
     }
-    
   });
-
 });
 
+
+app.get('/posts/:postId', (req, res) => {
+
+  const requestedPostId = req.params.postId;
+
+  Blog.findOne({ _id: requestedPostId }, (err, post) => {
+    if(!err){
+      res.render('post', { title: post.title, content: post.body, author: post.author });
+      } else {
+        console.log(`Post not found! ${err}`);
+      }
+  });
+});
 
 
 app.listen(3000, function() {
